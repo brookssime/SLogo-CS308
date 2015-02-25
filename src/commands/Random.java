2f104f2f4d098dc8bc9a28e98b0122ad26d06995
Thomas Puglisi <tom.puglisi@gmail.com>
2015-02-25 11:58:01 -0500

    Added the Minus, Random, and Remainder commands

---
+++ src/commands/Random.java
@@ -0,0 +1,20 @@
+package commands;
+
+import java.util.List;
+
+import application.Model;
+
+public class Random extends Command {
+    public static java.util.Random rand;;
+    
+    public Random(Model myModel) {
+        super(myModel, 1);
+        rand = new java.util.Random();
+    }
+
+    @Override
+    public List<Object> function(List<Object> args) {
+        return putDoubleInList(rand.nextDouble()*((double)args.get(0)));
+    }
+
+}
