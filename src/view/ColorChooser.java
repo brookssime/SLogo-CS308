303b87edbf060c5458114ab00ff4f05d968da537
Cosette Goldstein <cosette.goldstein@duke.edu>
2015-02-25 01:55:37 -0500

    added filechooser for selecting and image for the turtle, and edited display class to correct editing of pen color. Created a sample of movement by clicking language button

--- src/view/ColorChooser.java
+++ src/view/ColorChooser.java
@@ -1,53 +1,59 @@
 package view;
 
-import javafx.application.Application;
-import javafx.event.*;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
-import javafx.geometry.Insets;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
-
-
 public class ColorChooser{
- 
-	
 	private Color penColor;
 	private Color backgroundColor;
-	
-	public ColorChooser() {
+	private Display disp;
+	public ColorChooser(Display d) {
 		penColor=Color.BLACK;
 		backgroundColor=Color.WHITE;
+		disp=d;
 	}
-    
-    public HBox formatColorSelector(String object){
-    	HBox box=new HBox();
-    	final ColorPicker colorOfObject = new ColorPicker();
-        
-    	colorOfObject.setValue(penColor);
-        
-        final Text displayColor = new Text("Color of "+object);
-        displayColor.setFont(Font.font ("Verdana", 20));
-        displayColor.setFill(colorOfObject.getValue());
-        
-        
-        colorOfObject.setOnAction(e->
-                displayColor.setFill(colorOfObject.getValue()));
-        box.getChildren().addAll(colorOfObject,displayColor);
-    	
-    	return box;
-    }
-	
+	public HBox formatColorSelector(String object){
+		HBox box=new HBox();
+		final ColorPicker colorOfObject = new ColorPicker();
+		final Text displayColor = new Text("Color of "+object);
+		displayColor.setFont(Font.font ("Verdana", 16));
+		if (object.equals("Pen")){
+			colorOfObject.setValue(penColor);
+			colorOfObject.setOnAction(e-> {
+				displayColor.setFill(colorOfObject.getValue());
+				disp.setLineFill(colorOfObject.getValue());
+				penColor=colorOfObject.getValue();
+			}
+					);
+		}
+		else {
+			colorOfObject.setValue(backgroundColor);
+			colorOfObject.setOnAction(e-> {
+				displayColor.setFill(colorOfObject.getValue());
+				disp.setBackgroundFill(colorOfObject.getValue());
+				backgroundColor=colorOfObject.getValue();
+			}
+					);
+		}
+		displayColor.setFill(colorOfObject.getValue());
+		box.getChildren().addAll(displayColor,colorOfObject);
+		return box;
+	}
 	public void makeColorChooserPopUp(Stage stage) {
-        stage.setTitle("ColorPicker");
-        Scene scene = new Scene(new VBox(20), 400, 100);
-        VBox box = (VBox) scene.getRoot();
-        
-        box.getChildren().addAll(formatColorSelector("Pen"),formatColorSelector("Background"));
-        stage.setScene(scene);
-        stage.show();
-    }
+		stage.setTitle("ColorPicker");
+		Scene scene = new Scene(new VBox(20), 400, 100);
+		VBox box = (VBox) scene.getRoot();
+		box.getChildren().addAll(formatColorSelector("Pen"),formatColorSelector("Background"));
+		//stage.setOnCloseRequest(e->disp.setBackgroundFill(backgroundColor));
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	protected Display getDisplay() {
+		return disp;
+	}
 }
---
+++ src/view/ImageChooser.java
@@ -0,0 +1,58 @@
+package view;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class ImageChooser {
+
+	private Desktop desktop = Desktop.getDesktop();
+	private Display display;
+	public ImageChooser(Display d) {
+		display=d;
+	}
+	
+	public void start(final Stage stage) {
+		final FileChooser fileChooser = new FileChooser();
+		
+		File file = fileChooser.showOpenDialog(stage);
+		if (file != null) {
+			openFile(file);
+		}
+
+
+		final GridPane inputGridPane = new GridPane();
+
+		final Pane rootGroup = new VBox(12);
+		rootGroup.getChildren().addAll(inputGridPane);
+		rootGroup.setPadding(new Insets(12, 12, 12, 12));
+	}
+
+	private void openFile(File file) {
+				ImageView img=display.updateTurtleImage(); 
+				try {
+					Image image = SwingFXUtils.toFXImage(ImageIO.read(file), null);
+	                img.setFitHeight(30);
+	                img.setFitWidth(30);
+	                img.setImage(image);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+ 
+	}
+}
+
+
