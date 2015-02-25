c5b3b86251fa65ce18aabd7ffe0172339194161a
Sean Scott <sas75@duke.edu>
2015-02-24 23:37:51 -0500

    Fixed Forward command

--- src/commands/Forward.java
+++ src/commands/Forward.java
@@ -3,6 +3,7 @@ package commands;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 import application.Model;
 import application.Turtle;
 
@@ -15,68 +16,57 @@ public class Forward extends Command {
     }
     @Override
     public List<Object> function(List<Object> args) {
-        maxX = myModel.getMaxX();
+    	double distance = (double) args.get(0);
+        advanceTurtle(distance);
+        return putDoubleInList(distance);
+    }
+	private void advanceTurtle(double distance) {
+		maxX = myModel.getMaxX();
         maxY = myModel.getMaxY();
         Turtle turtle = myModel.getActiveTurtle();
-        double distance = (double) args.get(0);
         double radiansHeading = turtle.getRadiansHeading();
         double x = turtle.getX() + distance*Math.cos(radiansHeading);
         double y = turtle.getY() + distance*Math.sin(radiansHeading);
-        List<Object> list = new ArrayList<>();
+        boolean temp = turtle.isPenDown();
         if (outOfXBounds(x)) {
-            boolean temp = turtle.isPenDown();
-            if (x > 0) {
-                x -= maxX;
-                list.add(maxX/Math.cos(radiansHeading));
-                function(list);
-                turtle.setPenDown(false);
-                turtle.setX(-maxX);
-                turtle.setPenDown(temp);
-                list.clear();
-                list.add(x/Math.cos(radiansHeading));
-                function(list);
-            } else {
-                x += maxX;
-                list.clear();
-                list.add(-maxX/Math.cos(radiansHeading));
-                function(list);
-                turtle.setPenDown(false);
-                turtle.setX(maxX);
-                turtle.setPenDown(temp);
-                list.clear();
-                list.add(x/Math.cos(radiansHeading));
-                function(list);
+            double signX = Math.signum(x);
+            double distX = signX*maxX - turtle.getX();
+            double incrDist = distX/Math.cos(radiansHeading);
+            double incrY = turtle.getY() + incrDist*Math.sin(radiansHeading);
+            if(outOfYBounds(incrY)){
+                double signY = Math.signum(y);
+                double distY = signY*maxY - turtle.getY();
+                incrDist = distY/Math.sin(radiansHeading);
+                double incrX = turtle.getX() + incrDist*Math.cos(radiansHeading);
+            	turtle.setLocation(incrX, signY*maxY);
+            	turtle.setPenDown(false);
+            	turtle.setY(-signY*maxY);
+            	turtle.setPenDown(temp);
+            	advanceTurtle(distance - incrDist);
             }
-        } else if (outOfYBounds(y)) {
-            boolean temp = turtle.isPenDown();
-            if (y > 0) {
-                y -= maxY;
-                list.clear();
-                list.add(maxY/Math.sin(radiansHeading));
-                function(list);
-                turtle.setPenDown(false);
-                turtle.setX(-maxY);
-                turtle.setY(Math.tan(radiansHeading));
-                turtle.setPenDown(temp);
-                list.clear();
-                list.add(y/Math.sin(radiansHeading));             
-                function(list);          
-            } else {
-                y += maxY;
-                list.clear();
-                list.add(-maxY/Math.sin(radiansHeading));
-                function(list);
-                turtle.setPenDown(false);
-                turtle.setY(maxY);
-                turtle.setPenDown(temp);
-                list.clear();
-                list.add(y/Math.sin(radiansHeading));
-                function(list);
+            else{
+            	turtle.setLocation(signX*maxX, incrY);
+            	turtle.setPenDown(false);
+            	turtle.setX(-signX*maxX);
+            	turtle.setPenDown(temp);
+            	advanceTurtle(distance - incrDist);
             }
         }
-        turtle.setLocation(x, y);
-        return putDoubleInList(distance);
-    }
+        else if (outOfYBounds(y)){
+            double signY = Math.signum(y);
+            double distY = signY*maxY - turtle.getY();
+            double incrDist = distY/Math.sin(radiansHeading);
+            double incrX = turtle.getX() + incrDist*Math.cos(radiansHeading);
+            turtle.setLocation(incrX, signY*maxY);
+        	turtle.setPenDown(false);
+        	turtle.setY(-signY*maxY);
+        	turtle.setPenDown(temp);
+        	advanceTurtle(distance - incrDist);
+        }
+        else{
+        	turtle.setLocation(x, y);
+        }
+	}
     
     private boolean outOfXBounds(double x) {
         return (Math.abs(x) > maxX);
