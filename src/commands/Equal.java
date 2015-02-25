d2ea15b98cdf0f9740ed28d9ac93136c94abc7c1
Sean Scott <sas75@duke.edu>
2015-02-25 13:04:37 -0500

    Added Back and Boolean operations

---
+++ src/commands/Equal.java
@@ -0,0 +1,21 @@
+package commands;
+
+import java.util.List;
+
+import application.Model;
+
+public class Equal extends Command{
+
+	public Equal(Model model) {
+		super(model, 2);
+	}
+
+	@Override
+	public List<Object> function(List<Object> args) {
+		boolean eq = (double)args.get(0) == (double)args.get(1);
+		double eqNum = -1;
+		if (eq) eqNum = 1;
+		return putDoubleInList(eqNum);
+	}
+
+}
---
+++ src/commands/NotEqual.java
@@ -0,0 +1,19 @@
+package commands;
+
+import java.util.List;
+
+import application.Model;
+
+public class NotEqual extends Command{
+
+	public NotEqual(Model model) {
+		super(model, 2);
+	}
+
+	@Override
+	public List<Object> function(List<Object> args) {
+		double eqNum = (double)new Equal(myModel).function(args).get(0);
+		return putDoubleInList(-eqNum);
+	}
+
+}
