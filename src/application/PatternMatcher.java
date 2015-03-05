b739b5e1cef692713a83f22826cb28139e58cf6a
Thomas Puglisi <tom.puglisi@gmail.com>
2015-03-04 23:39:34 -0500

    Extracted PatternMatcher from Parser

---
+++ src/application/PatternMatcher.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+public class PatternMatcher {
+    private static boolean match(String input, Pattern regex) {
+        // THIS IS THE KEY LINE
+        return regex.matcher(input).matches();
+        // basic strings can match also, but not using a Pattern
+        // return input.matches(regex);
+    }
+
+    /**
+     * Checks if the given String falls under any of the given patterns
+     * 
+     * @param s
+     * @param patterns
+     * @return the pattern name that the String matches, or null if it does not
+     *         match a pattern
+     */
+    public static String checkForMatch(String s,
+            List<Map.Entry<String, Pattern>> patterns) {
+        for (Map.Entry<String, Pattern> p : patterns) {
+            if (match(s, p.getValue())) {
+                return p.getKey();
+            }
+        }
+        // Maybe throw an exception here
+        return null;
+    }
+
+    public static List<Map.Entry<String, Pattern>> makePatterns(String syntax) {
+        ResourceBundle resources = ResourceBundle.getBundle(syntax);
+        List<Map.Entry<String, Pattern>> patterns = new ArrayList<>();
+        Enumeration<String> iter = resources.getKeys();
+        while (iter.hasMoreElements()) {
+            String key = iter.nextElement();
+            String regex = resources.getString(key);
+            patterns.add(new AbstractMap.SimpleEntry<String, Pattern>(key,
+            // THIS IS THE KEY LINE
+                    Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
+        }
+        return patterns;
+    }
+}
