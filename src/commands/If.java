cbf471898d81ab6fc192f22f5fa21f58b05219d2
Thomas Puglisi <tom.puglisi@gmail.com>
2015-03-06 01:11:23 -0500

    Implemented the If and IfElse commands

---
+++ src/commands/If.java
@@ -0,0 +1,35 @@
+package commands;
+
+import java.util.List;
+
+import application.Model;
+import tree.BlockNode;
+import tree.CommandNode;
+import tree.EvaluatorNode;
+import tree.TreeNode;
+
+public class If extends CommandNode {
+
+    public If(Model myModel) {
+        super(myModel, double.class, BlockNode.class);
+    }
+
+    @Override
+    protected List<Object> function(List<Object> args) {
+        Object result = (double) 0;
+        if ((double) args.get(0) != (double) 0) {
+            result = evaluateCommand(args.get(1));
+        }
+        return putObjectInList(result);
+    }
+
+    protected Object evaluateCommand(Object o) {
+        Object result;
+        List<TreeNode> nodeList = getRootNodes(o);
+        EvaluatorNode evalNode = new EvaluatorNode(myModel, nodeList);
+        List<Object> tempList =  evalNode.evaluate();
+        result = tempList.get(tempList.size() - 1);
+        return result;
+    }
+
+}
---
+++ src/commands/IfElse.java
@@ -0,0 +1,26 @@
+package commands;
+
+import java.util.List;
+
+import tree.BlockNode;
+import application.Model;
+
+public class IfElse extends If {
+
+    public IfElse(Model myModel) {
+        super(myModel);
+        setParameterArray(double.class, BlockNode.class, BlockNode.class);
+    }
+    
+    @Override
+    protected List<Object> function(List<Object> args) {
+        Object result = (double) 0;
+        if ((double) args.get(0) != (double) 0) {
+            result = evaluateCommand(args.get(1));
+        } else {
+            result = evaluateCommand(args.get(2));
+        }
+        return putObjectInList(result);
+    }
+
+}
