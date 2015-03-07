18fcdb7d66d21e0ca081026adbe02fb6382a2949
Sean Scott <sas75@duke.edu>
2015-03-07 00:03:59 -0500

    added listeners for new commands

--- src/application/Turtle.java
+++ src/application/Turtle.java
@@ -16,6 +16,7 @@ public class Turtle {
     private ObjectProperty<double[]> location = new SimpleObjectProperty<>();
     private BooleanProperty penDown = new SimpleBooleanProperty();
     private BooleanProperty showing = new SimpleBooleanProperty();
+    private DoubleProperty shape = new SimpleDoubleProperty();
     private ObjectProperty<Node> node = new SimpleObjectProperty<>();
 
     public Turtle() {
@@ -93,8 +94,8 @@ public class Turtle {
         this.showing.setValue(showing);
     }
     
-    public Property<Node> getNodeProperty() {
-        return node;
+    public DoubleProperty shapeProperty(){
+    	return shape;
     }
 
     public Node getNode() {
